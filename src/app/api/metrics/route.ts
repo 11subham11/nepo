7ab7bc0c814712ac9628935195f41acdae98b871
SUBHAM MISHRA <subhammishra133@gmail.com>
2025-09-07 14:59:09 +0545

    Update

--- src/app/api/metrics/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { DEMO_USER_ID, getMetrics } from "@/lib/store";
-
-export async function GET() {
-	const data = getMetrics(DEMO_USER_ID);
-	return NextResponse.json(data);
-}
-
-
